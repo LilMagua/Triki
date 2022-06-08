4d8f40111a440cd8b0f1be202ff406116030cdb7
LilMagua <javieralextor@gmail.com>
2022-06-07 22:29:00 -0500

    Parte FrontEnd SemiTerminada

--- Js/Programacion.js
+++ Js/Programacion.js
@@ -5,10 +5,10 @@ var Jugador = 1;
 var Botones = Array.from(document.getElementsByTagName("button"));
 
 for(var i = 0; i<Botones.length; i++){
-    Botones[i].addEventListener("click",prueba)
+    Botones[i].addEventListener("click",Turno)
 }
 
-function prueba(event){
+function Turno(event){
     var boton_pulsado = event.target;
     if(Jugador == 1){
         boton_pulsado.innerHTML = "X";
@@ -26,85 +26,85 @@ function prueba(event){
 
 let Div_Nombre_Ganador = document.getElementById("Nombre_Ganador"); 
 function Verificar_Estado(){
+    var Boton0 = Botones[0];
+    var Boton1 = Botones[1];
+    var Boton2 = Botones[2];
+    var Boton3 = Botones[3];
+    var Boton4 = Botones[4];
+    var Boton5 = Botones[5];
+    var Boton6 = Botones[6];
+    var Boton7 = Botones[7];
+    var Boton8 = Botones[8];
+
     //Victorias por filas
-    if(Botones[0].value != "" && Botones[0].value == Botones[1].value && Botones[1].value == Botones[2].value){
-        Limpiar(Botones[0].value);
+    if(Boton0.value != "" && Boton0.value == Boton1.value && Boton1.value == Boton2.value){
+        Marcar(Boton0.value, Boton0, Boton1, Boton2);
     }
     else if(Botones[3].value != "" && Botones[3].value == Botones[4].value && Botones[4].value == Botones[5].value){
-        if(Botones[0].value == "X"){
-            alert("Gano el jugador 1 por fila 1");     
-        }else{
-            alert("Gano el jugador 2 por fila 1");     
-        }
+        Marcar(Boton0.value, Boton3, Boton4, Boton5);
     }
     else if(Botones[6].value != "" && Botones[6].value == Botones[7].value && Botones[7].value == Botones[8].value){
-        if(Botones[0].value == "X"){
-            alert("Gano el jugador 1 por fila 2");     
-        }else{
-            alert("Gano el jugador 2 por fila 2");     
-        }
+        Marcar(Boton0.value, Boton6, Boton7, Boton8);
     }
 
     //Victorias por columnas
     else if(Botones[0].value != "" && Botones[0].value == Botones[3].value && Botones[3].value == Botones[6].value){
-        if(Botones[0].value == "X"){
-            alert("Gano el jugador 1 por columna 0");     
-        }else{
-            alert("Gano el jugador 2 por columna 0");     
-        }
+        Marcar(Boton0.value, Boton0, Boton3, Boton6);
     }
     else if(Botones[1].value != "" && Botones[1].value == Botones[4].value && Botones[4].value == Botones[7].value){
-        if(Botones[0].value == "X"){
-            alert("Gano el jugador 1 por columna 1");     
-        }else{
-            alert("Gano el jugador 2 por columna 1");     
-        }
+        Marcar(Boton0.value, Boton1, Boton4, Boton7);
     }
     else if(Botones[2].value != "" && Botones[2].value == Botones[5].value && Botones[5].value == Botones[8].value){
-        if(Botones[0].value == "X"){
-            alert("Gano el jugador 1 por columna 2");     
-        }else{
-            alert("Gano el jugador 2 por columna 2");     
-        }
+        Marcar(Boton0.value, Boton2, Boton5, Boton8);
     }
 
     //Victorias en diagonal
     else if(Botones[0].value != "" && Botones[0].value == Botones[4].value && Botones[4].value == Botones[8].value){
-        if(Botones[0].value == "X"){
-            alert("Gano el jugador 1 por columna 2");     
-        }else{
-            alert("Gano el jugador 2 por columna 2");     
-        }
+        Marcar(Boton0.value, Boton0, Boton4, Boton8);
     }
     else if(Botones[2].value != "" && Botones[2].value == Botones[4].value && Botones[4].value == Botones[6].value){
-        if(Botones[0].value == "X"){
-            alert("Gano el jugador 1 por columna 2");     
-        }else{
-            alert("Gano el jugador 2 por columna 2");     
-        }
+        Marcar(Boton0.value, Boton2, Boton4, Boton6);
     }
 }
 
 
 
-function Limpiar(Ganador){
+function Marcar(Ganador,Primer_B,Segundo_B,Tercer_B){
     if(Ganador == "X"){
         Div_Nombre_Ganador.innerHTML = "Gana el Jugador 1";
-        Botones[0].classList.add("Triki");
-        Botones[1].classList.add("Triki");
-        Botones[2].classList.add("Triki");
+        Primer_B.classList.add("Triki");
+        Segundo_B.classList.add("Triki");
+        Tercer_B.classList.add("Triki");
     }else{
-        Div_Nombre_Ganador.innerHTML = "Gana el Jugador 1";
-        Botones[0].classList.add("Triki");
-        Botones[1].classList.add("Triki");
-        Botones[2].classList.add("Triki");
+        Div_Nombre_Ganador.innerHTML = "Gana el Jugador 2";
+        Primer_B.classList.add("Triki");
+        Segundo_B.classList.add("Triki");
+        Tercer_B.classList.add("Triki");    
     }
-    /*
+
+   setTimeout(Limpiar, 5000);
+}
+
+function Limpiar(){
     for(var i = 0; i<Botones.length; i++){
         Botones[i].innerHTML = "";
+        Botones[i].classList.remove("Triki");
+        Botones[i].value = "";
     }
-    */
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
